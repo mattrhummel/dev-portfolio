304a88d899ee3ed54f6b5e62c5667a69bb4bbcb5
mattrhummel <matt@matthummel.me>
2022-10-03 13:59:25 -0400

    update content

--- page-contact.php
+++ page-contact.php
@@ -28,17 +28,19 @@ get_header();
 
          <section class="light-background">
             <div class="container section-content">
-                    <div class="col padding-right-60">
-                        <?php the_content(); ?>      
-                    </div>
 
-                    <div class="col">
+
+            <div class="col">
                         <h2><?php _e('I would love to hear from you!', 'dev_portfolio') ?></h2>
                         <p><?php _e('Give me a call, send me and email, or use the form.', 'dev_portfolio') ?></p>
                         <p><?php _e('I am currently accepting new work.', 'dev_portfolio') ?></p>
 						<?php the_post_thumbnail(); ?>
                         <p><i class="fa fa-phone"></i><a href="tel:15407067647"><?php _e(' (540) 706- 7647') ?></a></p>
                         <p><i class="fa fa-envelope"></i><a href="mailto:matt@matthummel.com"> matt@matthummel.com</a></p>
+                    </div>
+
+                    <div class="col">
+                        <?php the_content(); ?>      
                     </div>
                 </div>  
             </div>
