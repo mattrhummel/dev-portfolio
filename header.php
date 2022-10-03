72f49610f6a40693933c7bf65ccfcbc766e41fd0
mattrhummel <matt@matthummel.me>
2022-10-03 17:38:58 -0400

    add google analytics tag

--- header.php
+++ header.php
@@ -14,6 +14,15 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V9718KN95J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V9718KN95J');
+</script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
