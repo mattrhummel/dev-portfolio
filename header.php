fa12ae7edf18f869e1ab374aeb51662d593e8e11
mattrhummel <matt@matthummel.me>
2022-10-04 00:51:22 -0400

    fix title

--- header.php
+++ header.php
@@ -11,7 +11,18 @@
 
 ?>
 <!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) & !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
+<!--<![endif]-->
 
 <head>
     <!-- Google tag (gtag.js) -->
@@ -23,7 +34,30 @@
 
   gtag('config', 'G-V9718KN95J');
 </script>
-<title><?php bloginfo('name');?></title>
+<title>
+<?php
+	// Print the <title> tag based on what is being viewed.
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+if ( $site_description && ( is_home() || is_front_page() ) ) {
+	echo " | $site_description";
+}
+
+	// Add a page number if necessary:
+if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+	/* translators: %s: Page number. */
+	echo esc_html( ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) ) );
+}
+
+?>
+	</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
