0878330dcce2a8cfe34b1cd13fe4d2b576d9e7af
mattrhummel <matt@matthummel.me>
2022-10-04 00:45:26 -0400

    fix charset issue

--- header.php
+++ header.php
@@ -23,7 +23,6 @@
 
   gtag('config', 'G-V9718KN95J');
 </script>
-    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
