d5ce7ee3e9d60d9c026574b4f3c8b49f790874e0
mattrhummel <matt@matthummel.me>
2022-10-11 22:56:55 -0400

    add projects section

--- index.php
+++ index.php
@@ -56,6 +56,59 @@ get_header();
 
 <?php endif; ?>
 
+<section class="light-background">
+
+	<div class="container-block">
+
+		<header class="text-center">
+			<h1><?php _e('Projects', 'dev-portfolio') ?></h1>
+			<h2 class="subheading"><?php _e('My most recent projects', 'dev-portfolio') ?></h2>
+		</header>
+
+		<div class="card-container">
+
+	<?php $args = array(  
+        'post_type' => 'projects',
+        'post_status' => 'publish',
+		'posts_per_page' => 3,
+
+    
+);
+
+$loop = new WP_Query( $args ); 
+    
+while ( $loop->have_posts() ) : $loop->the_post(); 
+
+             ?>
+
+			<div class="card">
+
+				<?php if ( has_post_thumbnail() ) {
+                $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'normal' );
+                if ( ! empty( $large_image_url[0] ) ) {
+                    echo '<a href="' . esc_url( $large_image_url[0] ) . '" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '" class="responsive">';
+                    echo get_the_post_thumbnail( $post->ID, 'large' ); 
+                    echo '</a>';
+                }
+            } ?>
+
+				<h3><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h3>
+
+
+				<footer class="entry-footer">
+					<?php dev_portfolio_entry_footer(); ?>
+				</footer><!-- .entry-footer -->
+			</div>
+
+			<?php
+            endwhile;
+            wp_reset_postdata();  
+            ?>
+		</div>
+	</div>
+
+</section>
+
 
 <section class="light-background">
 
