211fe739924bcefb21967da9c6ea3d539858b8d2
mattrhummel <matt@matthummel.me>
2022-11-03 00:03:12 -0400

    add css validator icon

--- footer.php
+++ footer.php
@@ -23,12 +23,22 @@
 
 </main>
 
-
 <footer class="page-footer text-center dark-background">
 
         <p class="light-text">
                 Designed and Developed by <?php bloginfo('name'); ?>
         </p>
+
+        Valid CSS!
+
+<p>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+    </a>
+</p>
+        
 
 </footer>
 
