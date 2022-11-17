e2d04d26fa319dae3747319c8f15920ede771278
mattrhummel <matt@matthummel.me>
2022-11-17 13:11:30 -0500

    general updates

--- page-blog.php
+++ page-blog.php
@@ -47,6 +47,15 @@ while ( $loop->have_posts() ) : $loop->the_post();
 
             <div class="card">
 
+            <?php if ( has_post_thumbnail() ) {
+						$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'normal' );
+						if ( ! empty( $large_image_url[0] ) ) {
+							echo '<a href="' . esc_url( $large_image_url[0] ) . '" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '" class="responsive">';
+							echo get_the_post_thumbnail( $post->ID, 'large' ); 
+							echo '</a>';
+						}
+					} ?>
+
             <h3><a href="<?php echo get_the_permalink()?>"><?php the_title(); ?></a></h3>
 
                 <footer class="entry-footer">
