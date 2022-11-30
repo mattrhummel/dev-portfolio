6c8f173d7831ac5bd2422fe707f234ed011078b6
mattrhummel <matt@matthummel.me>
2022-11-30 17:10:01 -0500

    updates

--- header.php
+++ header.php
@@ -48,14 +48,14 @@
 
             </a>
 
-            <button href="javascript:void(0)" class="open-menu" onclick="openNav()" aria-label="menu-close">
+            <button href="javascript:void(0)" class="open-menu" onclick="openNav()" aria-label="menu-open">
                 <i class="fa-sharp fa-solid fa-bars"></i>
             </button>
 
             <nav id="popOutMenu" class="main-menu">
                         
-            <button href="javascript:void(0)" class="close-button" onclick="closeNav()"> <i class="fa-solid fa-xmark"></i>
-                        </button>
+                    <button href="javascript:void(0)" class="close-button" onclick="closeNav()" aria-label="menu-close"> <i class="fa-solid fa-xmark"></i>
+                    </button>
 
                     <div class="popout-container">
 
