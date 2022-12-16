381968af809d474120a776288d5f9e3f3cefcf71
mattrhummel <matt@matthummel.me>
2022-12-15 23:49:37 -0500

    update banner

--- header.php
+++ header.php
@@ -27,8 +27,7 @@
     <title><?php wp_title(''); ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <?php wp_head(); ?>
 </head>
 
