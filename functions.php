22f4cd9a1bceaea91fdc4736b420bdb53079e16b
mattrhummel <matt@matthummel.me>
2022-12-17 12:34:06 -0500

    remove unused jquery

--- functions.php
+++ functions.php
@@ -161,6 +161,14 @@ remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+/** * Completely Remove jQuery From WordPress */
+function my_init() {
+    if (!is_admin()) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', false);
+    }
+}
+add_action('init', 'my_init');
 /**
  * Enqueue scripts and styles.
  */
